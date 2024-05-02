641b2c60e49f369fca134f54f0b628c598c7cb04
Le Zhang <jiangshi8050@gmail.com>
2024-05-02 10:15:07 +0800

    ada

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getCategories } = require("../../client/src/utils/api_products");
+const { getCategories } = require("../controllers/category");
 
 router.get("/", async (req, res) => {
   try {
-    const categories = await getCategories()
+    const categories = await getCategories();
     res.status(200).send(categories);
   } catch (e) {
     res.status(400).send({ msg: e.message });
